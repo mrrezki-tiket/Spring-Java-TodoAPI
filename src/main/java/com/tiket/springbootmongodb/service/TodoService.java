2b2e2198239fde664c94390e21e0ad9f7870d88e
Muhammad Rezki Ananda <muhammad.ananda@tiket.com>
2022-04-08 03:05:14 +0700

    Refactoring to READ document

--- src/main/java/com/tiket/springbootmongodb/service/TodoService.java
+++ src/main/java/com/tiket/springbootmongodb/service/TodoService.java
@@ -4,7 +4,10 @@ import com.tiket.springbootmongodb.exception.TodoCollectionException;
 import com.tiket.springbootmongodb.model.TodoDTO;
 
 import javax.validation.ConstraintViolationException;
+import java.util.List;
 
 public interface TodoService {
     public void createTodo(TodoDTO todo) throws ConstraintViolationException, TodoCollectionException;
+
+    public List<TodoDTO> getAllTodos();
 }
--- src/main/java/com/tiket/springbootmongodb/service/TodoServiceImpl.java
+++ src/main/java/com/tiket/springbootmongodb/service/TodoServiceImpl.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,6 +26,16 @@ public class TodoServiceImpl implements TodoService {
         } else {
             todo.setCreatedAt(new Date(System.currentTimeMillis()));
             todoRepository.save(todo);
+        }
+    }
+
+    @Override
+    public List<TodoDTO> getAllTodos() {
+        List<TodoDTO> todos = todoRepository.findAll();
+        if (todos.size() > 0) {
+            return todos;
+        } else {
+            return new ArrayList<TodoDTO>();
         }
     }
 }
